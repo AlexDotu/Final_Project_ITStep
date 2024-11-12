d13000d491b787075798792f70149a583df85e0c
Alex Dotu <alexdotu@gmail.com>
2024-11-12 20:26:47 +0100

    Created 1.1.4.1 and 1.1.4.2. Updated other files

--- 1.1.2.4_route_test_Random_data_from_list_exceptions.py
+++ 1.1.2.4_route_test_Random_data_from_list_exceptions.py
@@ -11,15 +11,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--start-maximized")
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wait = WebDriverWait(driver, 20)
-
 excel_path = "CzechRepublicLocations.xls"
 locations_df = pd.read_excel(excel_path)
+
 
 def random_datetime_generator():
     random_date = datetime.now() + timedelta(days=random.randint(1, 7))
@@ -28,13 +22,18 @@ def random_datetime_generator():
     return format_date, random_time
 
 
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+wait = WebDriverWait(driver, 10)
+
 try:
     driver.get("https://www.idos.cz/")
 
     try:
         cookie_button = wait.until(EC.element_to_be_clickable((By.ID, "didomi-notice-agree-button")))
         cookie_button.click()
-
     except Exception as e:
         print("Cookie consent button not found or not clickable:", e)
 
@@ -44,12 +43,44 @@ try:
     odkud_input = wait.until(EC.visibility_of_element_located((By.ID, "From")))
     odkud_input.clear()
     odkud_input.send_keys(address_from)
-    odkud_input.send_keys(Keys.TAB)
+    odkud_input.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    try:
+        suggestion_list = wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, ".idos-autosuggest__suggestions-container"))
+        )
+        if suggestion_list:
+            first_suggestion = suggestion_list.find_element(By.CSS_SELECTOR, ".idos-autosuggest__suggestion")
+
+            first_suggestion.click()
+            print(
+                f"For \"{address_from}\" address clarification needed ! Selected the first suggestion from dropdown list.")
+        else:
+            pass
+    except Exception as e:
+        print(f"No address clarification needed for \"{address_from}\" or an error occurred: {e}")
 
     kam_input = wait.until(EC.visibility_of_element_located((By.ID, "To")))
     kam_input.clear()
     kam_input.send_keys(address_to)
-    kam_input.send_keys(Keys.TAB)
+    kam_input.send_keys(Keys.ARROW_DOWN)
+    kam_input.send_keys(Keys.ARROW_DOWN)
+    kam_input.send_keys(Keys.ENTER)
+
+    try:
+        suggestion_list = wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, ".idos-autosuggest__suggestions-container"))
+        )
+        if suggestion_list:
+            first_suggestion = suggestion_list.find_element(By.CSS_SELECTOR, ".idos-autosuggest__suggestion")
+            first_suggestion.click()
+            print(
+                f"For \"{address_to}\" address clarification needed! Selected the first suggestion from dropdown list.")
+        else:
+            pass
+    except Exception:
+        print(f"For \"{address_to}\" address clarification needed! Selected the first suggestion from dropdown list.")
 
     random_date, random_time = random_datetime_generator()
     print(f"Generated random date: {random_date}, time: {random_time}")
@@ -59,18 +90,11 @@ try:
 
     driver.execute_script("arguments[0].value = arguments[1];", date_input, random_date)
     driver.execute_script("arguments[0].value = arguments[1];", time_input, random_time)
-    time.sleep(1)
+    time.sleep(0.5)
 
     date_input.send_keys(Keys.ESCAPE)
-    time.sleep(0.5)
-
     date_input.send_keys(Keys.TAB)
     time_input.send_keys(Keys.TAB)
-    time.sleep(0.5)
-
-    odjezd_radioBox = driver.find_element(By.ID, "byArrival-departure")
-    odjezd_radioBox.send_keys(Keys.TAB)
-    time.sleep(1)
 
     hledat_button = wait.until(EC.element_to_be_clickable(
         (By.XPATH, "//button[contains(@class, 'btn btn-orange btn-small btn-shadow w-full')]")))
