3fd4478686403dd0a01152e0b79f8d389e9e55bb
Alexandr Dotsu <51963885+AlexDotu@users.noreply.github.com>
2024-12-22 16:22:46 +0100

    added Locations and Route_data

---
+++ Supporting_files/route_data.py
@@ -0,0 +1,20 @@
+address_from = 'Galandova 1237/11'
+address_to = 'Opletalova 1418/23'
+
+address_from_1 = 'Galandova 1237/11'
+address_to_1 = 'Olomouc hl.n.'
+
+address_from_2 = 'Ostrava'
+address_to_2 = 'Ústí nad Labem'
+
+address_from_3 = 'Praha'
+address_from_4 = 'Olomouc'
+address_to_3 = 'Brno'
+
+invalid_address_from = 'Gulandowa 1237/11'
+invalid_address_to = 'Blzen'
+empty_address = ''
+
+valid_login = 'alexandrudotu@gmail.com'
+valid_email = 'alexandrudotu@gmail.com'
+valid_password = 'IdosAlex'
